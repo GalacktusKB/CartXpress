ed25dd17d01d832ffdedae5464537b552f05d313
GalacktusKB <kuntalbarik.rajkhamar@gmail.com>
2025-07-16 17:59:39 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/cartxpress/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cartxpress
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/repository/screens/splash/splashscreen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:cartxpress/domain/constants/appcolors.dart';
+import 'package:cartxpress/repository/screens/login/loginscreen.dart';
+import 'package:cartxpress/repository/widgets/uihelper.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Loginscreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors().scaffoldbackground,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [UiHelper.CustomImage(img: "image 1 (1).png")],
+        ),
+      ),
+    );
+  }
+}
